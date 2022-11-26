053d73cdeed7f148318f4a9c240516ff35b9f655
Nattaphong <pr.nattaphong_st@tni.ac.th>
2022-11-26 11:28:58 +0700

    WorkshopA

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,20 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  //res.send('Hello kitty');
+  res.status(200).json(
+    {Fullname: 'Nattaphong Patis'})
+
 });
+
+router.get('/bio', function(req, res, next) {
+res.status(200).json(
+  {Fullname : 'Nattaphong Patis',
+   Nickname : 'Fuse',
+   Hobby    : 'Play game',
+   gitusername : 'Fuse-np'
+})
+});
+
 
 module.exports = router;
